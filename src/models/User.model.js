f76f2a2c1005a8f9cdd9834c40c2001b2cd8202e
Shakil771 <shakilhossaint58@gmail.com>
2026-07-23 00:47:10 +0600

    Last commit

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -66,11 +66,13 @@ const userSchema = new mongoose.Schema(
       default: true,
     },
     emailVerificationToken: String,
-    emailVerificationExpires: {
-      type: Date,
-      // default: () => new Date(Date.now() + ms(env.email.activation_token_expires_in)), // 10m from now
-      // expires: 0
-    },
+    emailVerificationExpires:Date,
+
+    // emailVerificationExpires: {
+    //   type: Date,
+    //   default: () => new Date(Date.now() + ms(env.email.activation_token_expires_in)), // 10m from now
+    //   expires: 0
+    // },
     passwordResetToken: String,
     passwordResetExpires: Date,
     passwordChangedAt: Date,
